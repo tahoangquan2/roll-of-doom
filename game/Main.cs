af217b4029168b86efb7886e1c9efb895dc15b24
Quan Hoang Ta <hoangquandt69@gmail.com>
2025-02-03 13:10:23 +0700

    feat: print game tree

--- game/Main.cs
+++ game/Main.cs
@@ -6,10 +6,30 @@ public partial class Main : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		CallDeferred(nameof(DeferredPrintTree));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void DeferredPrintTree()
+	{
+		PrintTree(GetTree().Root, 0);
+	}
+
+	private void PrintTree(Node node, int depth)
+	{
+		string indent = "";
+		for (int i = 0; i < depth; i++)
+		{
+			indent += "-   ";
+		}
+		GD.Print(indent + node.Name);
+		foreach (Node child in node.GetChildren())
+		{
+			PrintTree(child, depth + 1);
+		}
 	}
 }
